cf5caa7f092ebdff69722cc2fa9be350fa55bc3d
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-10-30 13:12:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 <?php
 
 require_once('connect.php');
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
 $sql = 'SELECT * FROM pracownicy';
 
