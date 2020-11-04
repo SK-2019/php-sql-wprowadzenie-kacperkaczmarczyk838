0c5fc1509a6221837ed0c75a1873ce50d00bebce
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-04 16:29:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
  
  <h1>Kacper Kaczmarczyk 2Ti numer 13 </h1>
- <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">Github</a><h3>
+ <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
 <?php
 
 require_once('connect.php');
