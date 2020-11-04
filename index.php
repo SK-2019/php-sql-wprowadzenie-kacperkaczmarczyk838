389a23a61569d0439c877edee21b3cfc4264e66e
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-04 14:54:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,9 @@ echo("<h2>Zadanie 1</h2>");
 echo("<li>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org</li>");
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org');
+ $result = $conn->query('SELECT id, imie, zarobki, data_urodzenia, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org');
         echo("<table border=1>");
+        echo("<th>ID</th>")
         echo("<th>Imie</th>");
         echo("<th>Zarobki</th>");
         echo("<th>Data_Urodzenia</th>");
