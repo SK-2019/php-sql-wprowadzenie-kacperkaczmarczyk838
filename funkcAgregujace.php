c551bc015f2cb264590b0d7db36fd2abecb773e9
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 09:02:26 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -1,5 +1,16 @@
-<?php
- <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
- <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+	<a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838"><b>GITHUB</b></a>	
+</body>
 
+	<div class="nav">
+		<a  class="nav1" href="funkcjeagr.php"><b>Funkcje Agregujące</b></a>
+		<a  class="nav2" href="pracownicy.php"><b>Pracownicy</b></a>
+	</div>
+
+<?php
+  echo("<div>MIŁEGO DNIA</div>");
 ?>
