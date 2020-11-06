86e72ed78a1560a38ef876ebdcfc8e202d13666c
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 15:01:57 +0100

    Update groupby.php

--- groupby.php
+++ groupby.php
@@ -15,21 +15,18 @@
 <?php
 
 require_once('connect.php');
-echo("<h2>Zadanie 1</h2>");
-echo("<li>SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org</li>");
+echo("<h1>Group By:</h1>");
+echo("<h2>Zadanie 1 - Suma zarobków w poszczególnych działach.</h2>");
+echo("<li>SELECT sum(zarobki) as suma FROM pracownicy group by dzial</li>");
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');
+ $result = $conn->query('SELECT sum(zarobki) as suma FROM pracownicy group by dzial');
         echo("<table>");
-        echo("<th>ID</th>");
-        echo("<th>Imie</th>");
+        echo("<th>Suma</th>");
         echo("<th>Dział</th>");
-        echo("<th>Zarobki</th>");
-        echo("<th>Data_Urodzenia</th>");
-        echo("<th>Nazwa_Działu</th>");
             while($row=$result->fetch_assoc()){ 
                 echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                    echo("<td>".$row["suma"]."</td><td>".$row["dzial"]."</td>"); 
 
                 echo("</tr>");
             }
