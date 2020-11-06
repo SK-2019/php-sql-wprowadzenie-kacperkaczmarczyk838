09661e0cf3c894cc6beeae8382557bb9c29b6d6e
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 14:58:50 +0100

    Update sortowanie.php

--- sortowanie.php
+++ sortowanie.php
@@ -8,9 +8,10 @@
  <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
  <div class="nav">
 <a class="nav1" href="index.php">Strona Główna</a>
-<a class="nav1" href="pracownicy.php">Pracownicy</a>
+<a class="nav4" href="pracownicy.php">Pracownicy</a>
 <a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
 <a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+<a class="nav5" href="groupby.php">Group by</a>
 </div>
 <?php
   
