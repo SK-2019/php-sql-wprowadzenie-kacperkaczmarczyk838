ccea0c985a624d9f4d0541e470124f78999f920c
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 14:55:31 +0100

    Update groupby.php

--- groupby.php
+++ groupby.php
@@ -13,3 +13,26 @@
 <a class="nav4" href="sortowanie.php">Sortowanie</a>
 </div>
 <?php
+
+require_once('connect.php');
+echo("<h2>Zadanie 1</h2>");
+echo("<li>SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org</li>");
+
+$conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
+ $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');
+        echo("<table>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dział</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  ?>
