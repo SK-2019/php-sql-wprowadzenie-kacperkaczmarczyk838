bc71402d3ae9fbbf0c21ea13083bb8021a91f6af
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 15:09:14 +0100

    Update groupby.php

--- groupby.php
+++ groupby.php
@@ -32,5 +32,24 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
             }
         echo("</table>");
 	 
+
+require_once('connect.php');
+echo("<hr />");
+echo("<h2>Zadanie 2 - Ilość pracowników w poszczególnych działach.</h2>");
+echo("<li>SELECT dzial, count(imie) as ilosc, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org group by dzial</li>");
+
+$conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
+ $result = $conn->query('SELECT dzial, count(imie) as ilosc, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org group by dzial');
+        echo("<table>");
+        echo("<th>Dział</th>");
+        echo("<th>Ilość</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["dzial"]."</td><td>".$row["ilosc"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                echo("</tr>");
+            }
+        echo("</table>");
+	 
   
   ?>
