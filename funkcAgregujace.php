08be27eefe0e8a10a4dd921614f6722e42a240eb
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 14:33:32 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -88,6 +88,25 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
             }
 
         echo("</table>");
+	 
+echo("<hr />");
+require_once('connect.php');
+echo("<h2>Zadanie 5 - Średnia zarobków mężczyzn z działu 1 i 2 .</h2>");
+echo("<li>SELECT dzial, avg(zarobki) as srednia, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial=id_org and imie not like %a and (dzial=1 or dzial=2) group by dzial</li>");
+
+$conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
+ $result = $conn->query('SELECT dzial, avg(zarobki) as srednia, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial=id_org and imie not like "%a" and (dzial=1 or dzial=2) group by dzial');
+        echo("<table>");
+        echo("<th>Dzial</th>");
+        echo("<th>Średnia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["dzial"]."</td><td>".$row["srednia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                echo("</tr>");
+            }
+
+        echo("</table>");
 ?>
  
 
