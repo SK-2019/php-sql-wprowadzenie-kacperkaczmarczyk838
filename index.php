74668ba8d613196018464bfdd9cf237190de4b58
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 15:24:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ echo("<h1>Strona Główna</h1>");
 echo("<h2>Tabela Wszystkich Pracowników:</h2>");
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
 $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');       
+        echo("<table>");      
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dział</th>");
@@ -34,7 +35,7 @@ $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = 
 
 echo("<hr />");
 require_once('connect.php');
-echo("<h2>STabela Funkcji Aregujących</h2>");
+echo("<h2>Tabela Funkcji Agregujących</h2>");
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
 $result = $conn->query('SELECT dzial, sum(zarobki) as suma, avg(zarobki) as srednia, min(zarobki) as min, max(zarobki) as max, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org group by dzial');
