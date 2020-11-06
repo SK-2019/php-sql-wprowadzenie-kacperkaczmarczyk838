7b0fd6cd52ea59e2527b103fc252069200dbec46
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 15:18:07 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -17,28 +17,6 @@
 
 require_once('connect.php');
 echo("<h1>Pracownicy:</h1>");
-echo("<h2>Tabela Wszystkich Pracowników:</h2>");
-
-$conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');
-        echo("<table>");
-        echo("<th>ID</th>");
-        echo("<th>Imie</th>");
-        echo("<th>Dział</th>");
-        echo("<th>Zarobki</th>");
-        echo("<th>Data_Urodzenia</th>");
-        echo("<th>Nazwa_Działu</th>");
-            while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
-
-                echo("</tr>");
-            }
-        echo("</table>");
-
-echo("<hr />");
-require_once('connect.php');
-echo("<h1>Pracownicy:</h1>");
 echo("<h2>Zadanie 1 - Pracownicy tylko z działu 2.</h2>");
 echo("<li>SELECT * FROM pracownicy WHERE dzial = 2</li>");
 
