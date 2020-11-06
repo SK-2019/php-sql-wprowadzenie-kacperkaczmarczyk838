6be901db2b63cfb25daf572ff1648fd4ad7b7ef5
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 13:14:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <a class="nav1" href="pracownicy.php">Pracownicy</a>
 <a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
 <a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
-<a class="nav3" href="sortowanie.php">Sortowanie</a>
+<a class="nav4" href="sortowanie.php">Sortowanie</a>
 </div>
 <?php
 
