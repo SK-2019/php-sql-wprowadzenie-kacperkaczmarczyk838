63b71dd9a757636c50e73f577f459ba328d3334f
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 09:03:59 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,1 +1,11 @@
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+	<a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838"><b>GITHUB</b></a>
+
+<?php
+  echo("<div>MIŁEGO DNIA</div>");
+?>
 
