a3e23ed24097811e5b2f1a36fb2b5ae234fa848c
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 13:16:53 +0100

    Update sortowanie.php

--- sortowanie.php
+++ sortowanie.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html> 
+<head>
+ <link rel="stylesheet" href="mystyles.css">
+</head>
+ 
+ <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+ <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
+ <div class="nav">
+<a class="nav2" href="index.php">Strona Główna</a>
+<a class="nav1" href="pracownicy.php">Pracownicy</a>
+<a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
+<a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+</div>
+<?php
+require_once('connect.php');
+echo("<h2>Zadanie 1</h2>");
+echo("<li>SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org</li>");
 
+$conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
+ $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');
+        echo("<table>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dział</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+
+                echo("</tr>");
+            }
+        echo("</table>");
+   
+   ?>
