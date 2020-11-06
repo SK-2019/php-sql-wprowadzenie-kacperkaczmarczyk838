877b3ee740eaf11684a141f82533eb0a0c577806
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 14:53:30 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -7,10 +7,10 @@
  <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
  <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
  <div class="nav">
-	<a class="nav1" href="funkcAgregujace.php">Funkcje Agregujace</a>
-	<a class="nav1" href="pracownicy.php">Pracownicy</a>
+	<a class="nav1" href="index.php">Strona Główna</a>
+	<a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
+	<a class="nav3" href="pracownicy.php">Pracownicy</a>
 	<a class="nav4" href="sortowanie.php">Sorotwanie</a>
-	<a class="nav2" href="index.php">Strona Główna</a>
 </div>
 <?php
 	 
