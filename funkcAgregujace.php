4ca662355c62c06ce30a57424a0c81fab3a586f8
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 13:57:09 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -7,27 +7,26 @@
  <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
  <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
  <div class="nav">
+	<a class="nav2" href="index.php">Strona Główna</a>
 	<a class="nav1" href="pracownicy.php">Pracownicy</a>
 	<a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
 	<a class="nav4" href="sortowanie.php">Sorotwanie</a>
-	<a class="nav2" href="index.php">Strona Główna</a>
 </div>	 
 <?php
 
 require_once('connect.php');
 echo("<h1>Funkcje Agregujące:</h1>");
-echo("<h2>Zadanie 1</h2>");
-echo("<li>SELECT dzial, sum(zarobki) as suma, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org</li>");
+echo("<h2>Zadanie 1 - Suma zarobków wszystkich pracowników.</h2>");
+echo("<li>SELECT dzial, sum(zarobki) as suma FROM pracownicy</li>");
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT dzial, sum(zarobki) as suma, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org group by dzial');
+ $result = $conn->query('SELECT dzial, sum(zarobki) as suma FROM pracownicy');
         echo("<table>");
         echo("<th>Dzial</th>");
         echo("<th>Suma</th>");
-        echo("<th>Nazwa_Działu</th>");
             while($row=$result->fetch_assoc()){ 
                 echo("<tr>");
-                    echo("<td>".$row["dzial"]."</td><td>".$row["suma"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                    echo("<td>".$row["dzial"]."</td><td>".$row["suma"]."</td>"); 
 
                 echo("</tr>");
             }
@@ -37,21 +36,17 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
 echo("<hr />");
 	 
 
-echo("<h2>Zadanie 2</h2>");
-echo("<li>SELECT dzial, sum(zarobki) as suma, avg(zarobki) as srednia, min(zarobki) as min, max(zarobki) as max, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org group by dzial</li>");
+echo("<h2>Zadanie 2 - Suma zarobków wszystkich kobiet.</h2>");
+echo("<li>SELECT dzial, sum(zarobki) as suma FROM pracownicy where imie like %a</li>");
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
-$result = $conn->query('SELECT dzial, sum(zarobki) as suma, avg(zarobki) as srednia, min(zarobki) as min, max(zarobki) as max, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org group by dzial');
+$result = $conn->query('SELECT dzial, sum(zarobki) as suma FROM pracownicy where imie like "%a"');
             echo("<table>");
             echo("<th>Dział</th>");
             echo("<th>Suma</th>");
-            echo("<th>Średnia</th>");
-            echo("<th>Min</th>");
-            echo("<th>Max</th>");
-            echo("<th>Nazwa_Działu</th>");
                 while($row = $result->fetch_assoc()) {
         echo("<tr>");
-        echo("<td>".$row['dzial']."</td><td>".$row['suma']."</td><td>".$row['srednia']."</td><td>".$row['min']."</td><td>".$row['max']."</td><td>".$row["nazwa_dzial"]."</td>");
+        echo("<td>".$row['dzial']."</td><td>".$row['suma']."</td>");
         echo("</tr>");
     }
     echo("</table>");
