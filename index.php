1027a1587257055e689e3f93357bcfb2e374a7df
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-06 08:23:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
  
  <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
  <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
+ <div class="nav">
+<a href="pracownicy.php">Pracownicy - wstęp</a>
+<a href="funkcAgregujace.php">Funkcje Agregujace</a>
+</div>
 <?php
 
 require_once('connect.php');
