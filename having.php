9eef44af3c97ff1e22544cddc834907bea339cac
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-08 20:09:55 +0100

    Create having.php

---
+++ having.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link rel="stylesheet" href="mystyles.css">
+</head>
+ 
+ <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+ <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
+ <div class="nav">
+	<a class="nav1" href="index.php">Strona Główna</a>
+	<a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
+	<a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+	<a class="nav4" href="sortowanie.php">Sorotwanie</a>
+	<a class="nav5" href="groupby.php">Group by</a>
+  <a class="nav6" href="having">Having</a>
+</div>	 
+<?php
+
+require_once('connect.php');
+echo("<h1>Having:</h1>");
+
+?>
