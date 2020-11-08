32550351018a80362f37f0ea9144abca1e4a62df
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-08 20:16:07 +0100

    Update sortowanie.php

--- sortowanie.php
+++ sortowanie.php
@@ -12,6 +12,7 @@
 <a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
 <a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
 <a class="nav5" href="groupby.php">Group by</a>
+<a class="nav6" href="having.php">Having</a>
 </div>
 <?php
   
