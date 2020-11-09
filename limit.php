f72930394988fab9669686f4c83609cb3596ed0a
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-09 17:45:08 +0100

    Update limit.php

--- limit.php
+++ limit.php
@@ -19,5 +19,26 @@
 
 require_once('connect.php');
 echo("<h1>LIMIT:</h1>");
+echo("<h2>Zadanie 1 - Dwóch najlepiej zarabiających pracowników z działu 4.</h2>");
+echo("<li>SELECT * from pracownicy, organizacja where dzial=id_org and dzial=4 order by zarobki desc limit 2</li>");
+
+$conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
+ $result = $conn->query('SELECT * from pracownicy, organizacja where dzial=id_org and dzial=4 order by zarobki desc limit 2');
+        echo("<table>");
+	echo("<th>ID</th>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
 
 ?>
