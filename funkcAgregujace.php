660ab489f87d070bcb6c1e823bc43bbf52c5d68e
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-09 18:37:11 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -93,7 +93,7 @@ echo("<h2>Zadanie 5 - Średnia zarobków mężczyzn z działu 1 i 2 .</h2>");
 echo("<li>SELECT avg(zarobki) as srednia from pracownic where imie not like %a and (dzial=1 or dzial=2)</li>");
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT avg(zarobki) as srednia from pracownic where imie not like %a and (dzial=1 or dzial=2)');
+ $result = $conn->query('SELECT avg(zarobki) as srednia from pracownic where imie not like "%a" and (dzial=1 or dzial=2)');
         echo("<table>");
         echo("<th>Średnia</th>");
             while($row=$result->fetch_assoc()){ 
@@ -127,7 +127,7 @@ echo("<h2>Zadanie 7 - Ile kobiet pracuje łącznie w działach 1 i 3?</h2>");
 echo("<li>SELECT count(imie) as ilosc from pracownicy where imie like %a and (dzial=1 or dzial=3)</li>");
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT count(imie) as ilosc from pracownicy where imie like %a and (dzial=1 or dzial=3)');
+ $result = $conn->query('SELECT count(imie) as ilosc from pracownicy where imie like "%a" and (dzial=1 or dzial=3)');
         echo("<table>");
         echo("<th>Ilość</th>");
             while($row=$result->fetch_assoc()){ 
