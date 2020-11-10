57b5229682965208f5308730bdc5a4a5f0d83aef
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-10 12:40:40 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -175,7 +175,7 @@ $result = $conn->query('SELECT dzial, max(YEAR(CURDATE()) - YEAR(data_urodzenia)
   
    require("connect.php");
   echo("<hr />");
-echo("<h2>Zadanie 10- Najmłodsi pracownicy z działu: handel i serwis.</h2>");
+echo("<h2>Zadanie 10 - Najmłodsi pracownicy z działu: handel i serwis (nazwa_dział, wiek).</h2>");
 echo("<li>SELECT dzial, min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as min, nazwa_dzial from pracownicy, organizacja where dzial=id_org and (nazwa_dzial=handel OR nazwa_dzial=serwis) group by dzial</li>");
 $result = $conn->query('SELECT dzial, min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as min, nazwa_dzial from pracownicy, organizacja where dzial=id_org and (nazwa_dzial="handel" OR nazwa_dzial="serwis") group by dzial');
         echo("<table border>");
@@ -190,5 +190,21 @@ $result = $conn->query('SELECT dzial, min(YEAR(CURDATE()) - YEAR(data_urodzenia)
    
            echo("</table>");
  
+    require("connect.php");
+  echo("<hr />");
+echo("<h2>Zadanie 11 - Najmłodsi pracownicy z działu: handel i serwis (imię, nazwa_dział, wiek).</h2>");
+echo("<li>SELECT dzial, min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as min, nazwa_dzial from pracownicy, organizacja where dzial=id_org and (nazwa_dzial=handel OR nazwa_dzial=serwis) group by dzial</li>");
+$result = $conn->query('SELECT dzial, min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as min, nazwa_dzial from pracownicy, organizacja where dzial=id_org and (nazwa_dzial="handel" OR nazwa_dzial="serwis") group by dzial');
+        echo("<table border>");
+        echo("<th>Imie</th>");
+        echo("<th>Wiek - najmłodsi</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                 echo("<tr>");
+                   echo("<td>".$row["imie"]."</td><td>".$row["min"]."</td><td>".$row["nazwa_dzial"]."</td>");                    
+                 echo("</tr>");
+            }
+   
+           echo("</table>");
 
 ?>
