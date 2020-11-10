543ffb725cbf14da182abffcf3e95bb7b9cc9479
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-10 17:57:37 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -193,7 +193,7 @@ $result = $conn->query('SELECT min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as mi
   echo("<hr />");
 echo("<h2>Zadanie 11 - Najmłodsi pracownicy z działu: handel i serwis (imię, nazwa_dział, wiek).</h2>");
 echo("<li>SELECT imie, min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as wiek, nazwa_dzial from pracownicy, organizacja where dzial=id_org and (nazwa_dzial=handel OR nazwa_dzial=serwis) group by nazwa_dzial</li>");
-$result = $conn->query('SELECT min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as wiek, nazwa_dzial, imie from pracownicy, organizacja where dzial=id_org and (nazwa_dzial="hande" OR nazwa_dzial="serwis") group by nazwa_dzial');
+$result = $conn->query('SELECT min(YEAR(CURDATE()) - YEAR(data_urodzenia)) as wiek, nazwa_dzial, imie from pracownicy, organizacja where dzial=id_org and (nazwa_dzial="handel" OR nazwa_dzial="serwis") group by nazwa_dzial');
         echo("<table border>");
         echo("<th>Imie</th>");
         echo("<th>Wiek_najmłodsi</th>");
