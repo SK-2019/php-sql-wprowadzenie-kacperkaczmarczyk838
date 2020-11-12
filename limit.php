37c7b6085c79d571620eb4265e33bed5975f8c8c
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-12 14:45:07 +0100

    Update limit.php

--- limit.php
+++ limit.php
@@ -43,11 +43,12 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
         echo("</table>");
 	 
 echo("<hr />");
+	   $sql = 'SELECT * from pracownicy, organizacja where dzial=id_org and (dzial=4 or dzial=2) order by zarobki desc limit 3';
 echo("<h2>Zadanie 2 - Trzy najlepiej zarabiające kobiety z działu 4 i 2.</h2>");
-echo("<li>SELECT * from pracownicy, organizacja where dzial=id_org and (dzial=4 or dzial=2) order by zarobki desc limit 3</li>");
+echo("<li>".$sql);
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT * from pracownicy, organizacja where dzial=id_org and (dzial=4 or dzial=2) order by zarobki desc limit 3');
+ $result = $conn->query($sql);
         echo("<table>");
 	echo("<th>ID</th>");
         echo("<th>Imie</th>");
@@ -66,11 +67,12 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
 	 
 
 echo("<hr />");
+	  $sql = 'Select * From pracownicy, organizacja where dzial=id_org order by data_urodzenia asc limit 1';
 echo("<h2>Zadanie 3 - Najstarszy pracownik.</h2>");
-echo("<li>Select * From pracownicy, organizacja where dzial=id_org order by data_urodzenia asc limit 1</li>");
+echo("<li>".$sql);
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('Select * From pracownicy, organizacja where dzial=id_org order by data_urodzenia asc limit 1');
+ $result = $conn->query($sql);
         echo("<table>");
 	echo("<th>ID</th>");
         echo("<th>Imie</th>");
