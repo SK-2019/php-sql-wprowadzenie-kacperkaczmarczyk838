dad51c73361b1146602c16a101d1c5684c82c027
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-12 14:35:33 +0100

    Update having.php

--- having.php
+++ having.php
@@ -21,11 +21,12 @@
 
 require_once('connect.php');
 echo("<h1>Having:</h1>");
+	  $sql = 'SELECT sum(zarobki) as suma, dzial, nazwa_dzial from pracownicy, organizacja where dzial=id_org GROUP BY dzial HAVING sum(zarobki)<28';
 echo("<h2>Zadanie 1 - Suma zarobków w poszczególnych działach mniejsza od 28.</h2>");
-echo("<li>SELECT sum(zarobki) as suma, nazwa_dzial from pracownicy, organizacja where dzial=id_org GROUP BY dzial HAVING sum(zarobki)<28</li>");
+echo("<li>".$sql);
 
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
- $result = $conn->query('SELECT sum(zarobki) as suma, dzial, nazwa_dzial from pracownicy, organizacja where dzial=id_org GROUP BY dzial HAVING sum(zarobki)<28');
+ $result = $conn->query($sql);
         echo("<table>");
         echo("<th>Suma</th>");
 	echo("<th>Dzial</th>");
