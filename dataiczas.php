1ce57a1491756f7903f8e9c036554d70006b1a6e
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-13 15:49:52 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -418,7 +418,16 @@ echo("</table>");
   
    require("connect.php");
   echo("<hr />");
-   $sql = 'SELECT DATE_FORMAT(data_urodzenia,"%W") as dzien, imie, data_urodzenia FROM pracownicy ORDER BY CASE WHEN dzien = "Monday" THEN 1 WHEN dzien = "Tuesday" THEN 2 WHEN dzien = "Wednesday" THEN 3 WHEN dzien= "Thursday" THEN 4 WHEN dzien = "Friday" THEN 5 WHEN dzien = "Saturday" THEN 6 WHEN dzien = "Sunday" THEN 7 END ASC';
+   $sql = 'SELECT DATE_FORMAT(data_urodzenia,"%W") as dzien, imie, data_urodzenia FROM pracownicy 
+   ORDER BY CASE 
+   WHEN dzien = "Monday" THEN 1 
+   WHEN dzien = "Tuesday" THEN 2 
+   WHEN dzien = "Wednesday" THEN 3 
+   WHEN dzien= "Thursday" THEN 4 
+   WHEN dzien = "Friday" THEN 5 
+   WHEN dzien = "Saturday" THEN 6 
+   WHEN dzien = "Sunday" THEN 7 
+   END ASC';
 echo("<h2>Zadanie 8 - Pracownicy z nazwami dni tygodnia, w których się urodzili od poniedziałku do niedzieli</h2>");
 echo("<li>".$sql);
 
@@ -464,21 +473,21 @@ echo("</table>");
                                   WHEN dzien = 'Sobota' THEN 6
                                   WHEN dzien = 'Niedziela' THEN 7
                              END ASC");
-                echo("<h2>".$sql2."</h2>");
+                echo("<li>".$sql2);
                 $conn = new mysqli("remotemysql.com","gQvQ0qIoDC","4HAPys5ynL","gQvQ0qIoDC");
                 $result=$conn->query($sql1);
                 $result=$conn->query($sql2);
                 include("connect.php");
-                        echo("<table border=1>");
+                        echo("<table border>");
                         echo("<th>dzien</th>");
                         echo("<th>ilosc</th>");
 
 
-                        while($row=$result->fetch_assoc()) {
-                                echo("<tr>");
-                                    echo("<td>".$row["dzien"]."</td><td>".$row["ilosc"]."</td>");
-                                echo("</tr>");
-                            }
-                        echo("</table>");
+while($row=$result->fetch_assoc()) {
+echo("<tr>");
+    echo("<td>".$row["dzien"]."</td><td>".$row["ilosc"]."</td>");
+echo("</tr>");
+                 }
+      echo("</table>");
   
 ?>
