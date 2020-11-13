03777cdc9317c5ffcfca86eb3e591f447dde4599
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-13 12:51:23 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -303,7 +303,7 @@ echo("</table>");
   
     require("connect.php");
   echo("<hr />");
-  SET lc_time_names = 'pl_PL';
+//   SET lc_time_names = 'pl_PL';
    $sql = 'SELECT DATE_FORMAT(CURDATE(), "%W")';
 echo("<h2>Zadanie 2 - Wypisz dzisiejszą nazwę dnia po polsku (np. poniedziałek).</h2>");
 echo("<li>".$sql);
