0941b75a307642637814740a610ae0541f880b9c
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-17 13:04:05 +0100

    Create formularz.php

---
+++ formularz.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+  <form action="strona.php" method="POST">
+     <input type="text" name="imie"></br>
+     <input type="text" name="dzial"></br>
+     <input type="text" name="zarobki"></br>
+     <input type="submit" value="wyślij do strona.php">
+  </form>
+  </body>
+</html>
