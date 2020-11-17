7128c68f5c092d8bda2a8a58d9ca745771d464df
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-17 13:15:34 +0100

    Update formularz.php

--- formularz.php
+++ formularz.php
@@ -4,10 +4,10 @@
 <body>
 
 <form action="strona.php" method="POST">
-    <input type="text" name="imie"></br>
-    <input type="text" name="dzial"></br>
-    <input type="text" name="zarobki"></br>
-    <input type="submit" value="Wyślij">
+   Imie: <input type="text" name="imie"></br>
+   Dział: <input type="text" name="dzial"></br>
+   Zarobki <input type="text" name="zarobki"></br>
+    <input type="submit" value="Dodaj">
 </form>
 
 </body>
