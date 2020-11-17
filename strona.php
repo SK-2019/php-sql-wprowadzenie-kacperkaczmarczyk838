80eff591d6c32dd737e71a7e24b34f7bc6e79346
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-17 13:35:09 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,7 +1,15 @@
+<html>
+        <head>
+        <link rel="stylesheet" href="mstyle.css">
+        </head>
+  
+</html> 
 <?php
 
 echo("witam :D");
 
 echo($_POST["imie"]);
+echo($_POST['dzial']);
+echo($_POST['zarobki']);
 
 ?>
