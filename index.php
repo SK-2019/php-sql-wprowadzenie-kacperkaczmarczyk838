8ef16afe5abaec5a18684d35787ac0b713b1fd3d
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-17 13:03:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
   
    <form action="strona.php" method="POST">
      <input type="text" name="imie"></br>
+     <input type="text" name="dzial"></br>
+     <input type="text" name="zarobki"></br>
      <input type="submit" value="wyślij do strona.php">
   </form>
   
