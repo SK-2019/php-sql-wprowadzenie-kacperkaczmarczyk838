28b53bfac80538be9264194c22c0d70d5fe60c06
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-17 13:08:04 +0100

    Update formularz.php

--- formularz.php
+++ formularz.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html> 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>Kacper Kaczmarczyk 2Ti</title>
+ <title>Kacper Kaczmarczyk 2Ti</title>
+ <link rel="stylesheet" href="mystyles.css">
 </head>
-<body>
-  <form action="strona.php" method="POST">
+ 
+ <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+ <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
+ <div class="nav">
+<a class="nav1" href="pracownicy.php">Pracownicy</a>
+<a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
+<a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+<a class="nav4" href="sortowanie.php">Sortowanie</a>
+<a class="nav5" href="groupby.php">Group by</a>
+<a class="nav6" href="having.php">Having</a>
+<a class="nav7" href="limit.php">LIMIT</a>
+<a class="nav8" href="dataiczas.php">Data i czas</a>
+</div>
+  
+   <form action="strona.php" method="POST">
      <input type="text" name="imie"></br>
      <input type="text" name="dzial"></br>
      <input type="text" name="zarobki"></br>
      <input type="submit" value="wyślij do strona.php">
   </form>
-  </body>
-</html>
