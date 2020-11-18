68bad367ee24717eca7dca1cecac75f6c456fad3
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-18 16:40:26 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,8 +1,23 @@
 <html>
         <head>
-		<h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+		 <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
+ <div class="nav">
+<a class="nav0" href="formularz.html">Formularz</a>
+<a class="nav1" href="pracownicy.php">Pracownicy</a>
+<a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
+<a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+<a class="nav4" href="sortowanie.php">Sortowanie</a>
+<a class="nav5" href="groupby.php">Group by</a>
+<a class="nav6" href="having.php">Having</a>
+<a class="nav7" href="limit.php">LIMIT</a>
+<a class="nav8" href="dataiczas.php">Data i czas</a>
+</div>
+         
+    <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+    <h1>Dodano:</h1>
         <link rel="stylesheet" href="mstyle.css">
         </head>
+
   
 </html> 
 <?php
