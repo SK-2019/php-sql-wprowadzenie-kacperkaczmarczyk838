ad33f55a00f9d38c71216074d8ff2565068fbcb9
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-20 13:48:41 +0100

    Create strona.php

---
+++ strona.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+		 <title>Kacper Kaczmarczyk 2Ti</title>
+		 <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
+<meta charset="UTF-8">
+ <link rel="stylesheet" href="mstyles.css">
+		 <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
+ <div class="nav">
+<a class="nav1" href="index.php">Strona Główna</a>
+<a class="nav0" href="formularz.html">Formularz</a>
+<a class="nav1" href="pracownicy.php">Pracownicy</a>
+<a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
+<a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+<a class="nav4" href="sortowanie.php">Sortowanie</a>
+<a class="nav5" href="groupby.php">Group by</a>
+<a class="nav6" href="having.php">Having</a>
+<a class="nav7" href="limit.php">LIMIT</a>
+<a class="nav8" href="dataiczas.php">Data i czas</a>
+</div>
+   
+    <h1>Dodano:</h1>
+        </head>
+
+  
+</html> 
+<?php
+
+echo("<li>imię: ".$_POST['imie']."</li>");
+echo("<li>dział: ".$_POST['dzial']."</li>");
+echo("<li>zarobki: ".$_POST['zarobki']."</li>");
+echo("<li>data urodzenia: ".$_POST['data_ur']."</li>");
+
+
+ require_once("connect.php");
+	$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_ur']."')";
+	
+if ($conn->query($sql) === TRUE) {
+        echo("<p class='precord'>Dodano</p>");
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+
+?>
