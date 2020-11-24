1f3ea7cb958158f8d039d95acb344e2b0252e556
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-24 12:59:43 +0100

    Update function.php

--- function.php
+++ function.php
@@ -5,9 +5,11 @@ echo("asd");
 function nazwaJakas($liczba){
 echo("<li>teraz działa funkcja ");
 echo("<li>test ".$liczba);
-for ($i=1;$i<$liczba;$i++){
+for($i=1;$i<$liczba;$i++){
 echo("<li>to jest w petli: ".$i);
    }
 }
+
+nazwaJakas(150);
 
 ?>
