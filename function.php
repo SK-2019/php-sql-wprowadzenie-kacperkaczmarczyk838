8eefd71a2c97cafd2122edecfb6995ad1a87921c
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-24 12:56:57 +0100

    Create function.php

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+
+echo("asd");
+
+function nazwaJakas($liczba){
+echo("<li>teraz działa funkcja ");
+echo("<li>test ".$liczba);
+for ($i=1;$i<$liczba;$i++){
+echo("<li>to jest w petli: ".$i);
+   }
+}
+
+?>
