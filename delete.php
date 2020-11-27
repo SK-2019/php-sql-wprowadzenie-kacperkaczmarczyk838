58989401d831b6391ff3ebb11a7406636864ab6e
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-27 08:54:00 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+      <title>Kacper Kaczmarczyk 2Ti</title>
       <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
      <meta charset="UTF-8">
     <link rel="stylesheet" href="mstyles.css">
@@ -31,7 +32,7 @@ if ($conn->query($sql) === TRUE) {
         echo("<h1 class='precord'>'Error: ' . $sql . '<br>' . $conn->error</h1>");
       }
       
-           
+           echo("<hr />");
            echo("<h1>Tabela Wszystkich Pracowników:</h1>");
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
 $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');       
