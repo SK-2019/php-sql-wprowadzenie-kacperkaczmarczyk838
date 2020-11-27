de6ca47269b4a07da9b69641723eb3b9c5346239
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-11-27 08:51:29 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -41,6 +41,8 @@ if ($conn->query($sql) === TRUE) {
         echo "Error: " . $sql . "<br>" . $conn->error;
       }
 	
+	
+	echo("<hr />");
 	echo("<h1>Tabela Wszystkich Pracowników:</h1>");
 $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
 $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');       
