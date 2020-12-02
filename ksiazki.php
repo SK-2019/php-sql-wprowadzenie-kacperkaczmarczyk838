8e0c181a28b9993d37059bb46aed230eb58c0268
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-02 15:06:47 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -26,7 +26,7 @@
  require_once('connect.php');
 echo("<h1>Książki i autorzy:</h1>");
 $sql = 'SELECT * FROM biblTytul';
-echo("<h2>Tytuł</h2>");
+echo("<h2>Tytuły:</h2>");
 echo("<li>".$sql);
 
  $result = $conn->query($sql);
@@ -43,7 +43,6 @@ echo("<li>".$sql);
         echo("</table>");
   
    require_once('connect.php');
-echo("<h1>Książki i autorzy:</h1>");
 $sql = 'SELECT * FROM biblAutor';
 echo("<h2>Autorzy:</h2>");
 echo("<li>".$sql);
@@ -61,5 +60,22 @@ echo("<li>".$sql);
 
         echo("</table>");
 
+  require_once('connect.php');
+$sql = 'SELECT * FROM biblAutor_biblTytul';
+echo("<h2>ID:</h2>");
+echo("<li>".$sql);
+
+ $result = $conn->query($sql);
+        echo("<table>");
+        echo("<th>ID</th>");
+        echo("<th>Tytuł</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id"]."</td><td>".$row["biblAutor_id"]."</td><td>".$row["biblTytul_id"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
         
 ?>
