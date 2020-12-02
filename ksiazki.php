a95fcd373a5411fffd0ba7ed2e09a87d91a5cff8
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-02 15:14:52 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -8,6 +8,7 @@
  <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
  <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
  <div class="nav">
+<a class="nav1" href="index.php">Strona Główna</a>
 <a class="nav0" href="function.php">Funkcja</a>
 <a class="nav0" href="danedobazy.php">DaneDoBazy</a>
 <a class="nav0" href="formularz.html">Formularz</a>
