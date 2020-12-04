70a8676bbc992881ef20bd01b1e1c057f06c4000
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-04 11:51:02 +0100

    Update and rename funkcAgregujace.php to zadania/funkcAgregujace.php

--- zadania/funkcAgregujace.php
+++ zadania/funkcAgregujace.php
@@ -2,7 +2,7 @@
 <html>
 <head>
  <title>Kacper Kaczmarczyk 2Ti</title>
- <link rel="stylesheet" href="mstyles.css">
+ <link rel="stylesheet" href="/mstyles.css">
 </head>
  
  <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
@@ -20,7 +20,7 @@
         <a class="nav8" href="dataiczas.php">Data i czas</a>
 </div>	 
 <?php
-require_once('connect.php');
+require_once('../connect.php');
 echo("<h1>Funkcje agregujące:</h1>");
 	 $sql = 'SELECT sum(zarobki) as suma from pracownicy';
 echo("<h2>Zadanie 1 - Suma zarobków wszystkich pracowników.</h2>");
@@ -40,7 +40,6 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
         echo("</table>");
 
 echo("<hr />");
-require_once('connect.php');
 	 	 $sql = 'SELECT sum(zarobki) as suma from pracownicy where imie like "%a"';
 echo("<h2>Zadanie 2 - Suma zarobków wszystkich kobiet.</h2>");
 echo("<li>".$sql);
@@ -58,7 +57,6 @@ $result = $conn->query($sql);
         echo("</table>");
 	 
 echo("<hr />");
-require_once('connect.php');
 	 	 $sql = 'SELECT sum(zarobki) as suma from pracownicy where imie not like "%a" and (dzial=2 or dzial=3)';
 echo("<h2>Zadanie 3 - Suma zarobków mężczyzn pracujących w dziale 2 i 3.</h2>");
 echo("<li>".$sql);
@@ -76,7 +74,6 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
         echo("</table>");
 	 
 echo("<hr />");
-require_once('connect.php');
 	  	 $sql = 'SELECT avg(zarobki) as srednia from pracownicy where imie not like "%a"';
 echo("<h2>Zadanie 4 - Średnia zarobków wszystkich mężczyzn.</h2>");
 echo("<li>".$sql);
@@ -94,7 +91,7 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
         echo("</table>");
 	 
 echo("<hr />");
-require_once('connect.php');
+
 	 	 $sql = 'SELECT avg(zarobki) as srednia from pracownicy where dzial=4';
 echo("<h2>Zadanie 5 - Średnia zarobków pracowników z działu 4.</h2>");
 echo("<li>".$sql);
@@ -112,7 +109,7 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
         echo("</table>");
 	 
 echo("<hr />");
-require_once('connect.php');
+
 	 	 $sql = 'SELECT avg(zarobki) as srednia from pracownicy where imie not like "%a" and (dzial=1 or dzial=2)';
 echo("<h2>Zadanie 6 - Średnia zarobków mężczyzn z działu 1 i 2.</h2>");
 echo("<li>".$sql);
@@ -130,7 +127,7 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
         echo("</table>");
 	 
 echo("<hr />");
-require_once('connect.php');
+
 	  $sql = 'SELECT count(imie) as ilosc FROM pracownicy';
 echo("<h2>Zadanie 7 - Ilu jest wszystkich pracowników?</h2>");
 echo("<li>".$sql);
@@ -148,7 +145,7 @@ $conn = new mysqli("remotemysql.com","17wQgisS2h","QCoNVtdlto","17wQgisS2h");
         echo("</table>");
 	 
 echo("<hr />");
-require_once('connect.php');
+
 	 	 	 $sql = 'SELECT count(imie) as ilosc from pracownicy where imie like "%a" and (dzial=1 or dzial=3)';
 echo("<h2>Zadanie 8 - Ile kobiet pracuje łącznie w działach 1 i 3?</h2>");
 echo("<li>".$sql);
