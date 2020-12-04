f1f4ef4993827e663d43fd587e03299cf4443c9c
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-04 12:06:21 +0100

    Update groupby.php

--- zadania/groupby.php
+++ zadania/groupby.php
@@ -8,16 +8,18 @@
  <h1>💎 Kacper Kaczmarczyk nr 13 💎</h1>
  <h3><a href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a><h3>
  <div class="nav">
+	 
 <a class="nav1" href="index.php">Strona Główna</a>
-<a class="nav0" href="danedobazy.php">DaneDoBazy</a>
-<a class="nav0" href="formularz.html">Formularz</a>
-<a class="nav1" href="pracownicy.php">Pracownicy</a>
-<a class="nav2" href="funkcAgregujace.php">Funkcje Agregujace</a>
-<a class="nav3" href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
-<a class="nav4" href="sortowanie.php">Sortowanie</a>
-<a class="nav6" href="having.php">Having</a>
-<a class="nav7" href="limit.php">LIMIT</a>
-<a class="nav8" href="dataiczas.php">Data i czas</a>
+<a class="nav0" href="/danedobazy/danedobazy.php">DaneDoBazy</a>
+<a class="nav0" href="/danedobazy/formularz.html">Formularz</a>
+<a class="nav1" href="/zadania/pracownicy.php">Pracownicy</a>
+<a class="nav3" href="/zadania/pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+<a class="nav4" href="/zadania/sortowanie.php">Sortowanie</a>
+<a class="nav6" href="/zadania/having.php">Having</a>
+<a class="nav7" href="/zadania/limit.php">LIMIT</a>
+<a class="nav8" href="/zadania/dataiczas.php">Data i czas</a>
+<a class="nav0" href="/zadania/function.php">Funkcja</a>
+<a class="nav0" href="/biblioteka/ksiazki.php">Książki</a>
 </div>
 <?php
 
