19f99d1eb818ef93c502b290adf13b43bd607277
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-04 12:52:00 +0100

    Update ksiazki.php

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -40,7 +40,7 @@ echo("<li>".$sql);
 
         echo("</table>");
   
-
+echo("<hr />");
 $sql = 'SELECT * FROM biblAutor';
 echo("<h2>Autorzy:</h2>");
 echo("<li>".$sql);
@@ -58,22 +58,16 @@ echo("<li>".$sql);
 
         echo("</table>");
 
-  
+echo("<hr />");
 $sql = 'SELECT * FROM biblTytul';
 echo("<h2>Tytuły:</h2>");
 echo("<li>".$sql);
 
- $result = $conn->query($sql);
-        echo("<table>");
-        echo("<th>ID</th>");
-        echo("<th>Tytuł</th>");
-            while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>"); 
-
-                echo("</tr>");
-            }
-
-        echo("</table>");
+echo("<select name='title' id='title'>");
+while($row=$result->fetch_assoc()){ 
+echo("<option value=".$row['id'].">".$row['tytul']."</option>");
+         
+        }
+    echo("</select>");
         
 ?>
