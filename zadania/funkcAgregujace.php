77037c0a9165511bc0e567a17a129d3bfd754d80
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-09 15:43:04 +0100

    Update funkcAgregujace.php

--- zadania/funkcAgregujace.php
+++ zadania/funkcAgregujace.php
@@ -20,7 +20,6 @@
         <li class="item_link"><a class="" href="/danedobazy/formularz.html">Formularz</a></li>
         <li class="item_link"><a class="" href="/zadania/pracownicy.php">Pracownicy</a></li>
         <li class="item_link"><a class="" href="/zadania/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
-        <li class="item_link"><a class="" href="/zadania/funkcAgregujace.php">Funkcje Agregujace</a></li>
         <li class="item_link"><a class="" href="/zadania/sortowanie.php">Sorotwanie</a></li>
         <li class="item_link"><a class="" href="/zadania/groupby.php">Group By</a></li>
         <li class="item_link"><a class="" href="/zadania/having.php">Having</a></li>
