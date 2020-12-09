beade2329c7d84c918804ffc2dbf64e70f1899a7
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-09 19:46:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -75,9 +75,10 @@ $result = $conn->query('SELECT dzial, sum(zarobki) as suma, avg(zarobki) as sred
     }
     echo("</table>");
 
+	 
+?>
 	       <img 
 					src="https://i.pinimg.com/564x/74/83/3f/74833fe8f8b4822016284f27a2a2bcbc.jpg"
 			</img>
-?>
   </div>
   </body>
