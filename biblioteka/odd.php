7b2a636fabd345aee9b653c64b92cc8cb9192042
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-11 13:18:09 +0100

    Create odd.php

---
+++ biblioteka/odd.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+      <title>Kacper Kaczmarczyk 2Ti</title>
+    
+     <meta charset="UTF-8">
+    <link rel="stylesheet" href="/mstyles.css">
+  <body>
+    <div class="container">
+      <div class="item colorRed">
+        <h1> ☆ Kacper Kaczmarczyk 2Ti ☆</h1>
+
+        <h1 class="tltle"></h1>
+      </div>
+      <div class="item colorBlue">
+     Menu Boczne:
+       <ul>
+        <li class="item_link"><a class="" href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a></li>
+        <li class="item_link"><a class="" href="/danedobazy/danedobazy.php">Dane Do Bazy</a></li>
+        <li class="item_link"><a class="" href="/danedobazy/formularz.html">Formularz</a></li>
+        <li class="item_link"><a class="" href="/zadania/pracownicy.php">Pracownicy</a></li>
+        <li class="item_link"><a class="" href="/zadania/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
+        <li class="item_link"><a class="" href="/zadania/funkcAgregujace.php">Funkcje Agregujace</a></li>
+        <li class="item_link"><a class="" href="/zadania/sortowanie.php">Sorotwanie</a></li>
+        <li class="item_link"><a class="" href="/zadania/groupby.php">Group By</a></li>
+        <li class="item_link"><a class="" href="/zadania/having.php">Having</a></li>
+        <li class="item_link"><a class="" href="/zadania/limit.php">Limit</a></li>
+        <li class="item_link"><a class="" href="/zadania/dataiczas.php">Data i Czas</a></li>
+        <li class="item_link"><a class="" href="/zadania/function.php">Funkcja</a></li>
+        <li class="item_link"><a class="" href="/biblioteka/ksiazki.php">Książki</a></li>
+       </ul>
+       
+    </div>
+      <div class="item colorGreen">
+         
+      <?php
+
+  echo ("<h1>id: ".$_POST['id']."</h1>");
+require_once("../connect.php");
+
+ $sql = "DELETE FROM biblAutor, biblTytul, biblAutor_biblTytul WHERE id='".$_POST['id']."'";
+
+if ($conn->query($sql) === TRUE) {
+        echo("<h1 class='precord'> Usunięto pracownika </h1>");
+       header('Location: https://php-test-tabd.herokuapp.com/index.php');
+      } else {
+        echo("<h1 class='precord'>'Error: ' . $sql . '<br>' . $conn->error</h1>");
+      }
+      
+
+
+?>
+
+</body>
+</head>
+</html>
+</div>
+  </body>
