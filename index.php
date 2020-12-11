94b66c84e816894345e0ac9d436b0860bd1fbbd6
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-11 20:11:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
  <title>Kacper Kaczmarczyk 2Ti </title>
- <link rel="stylesheet" href="/mystyles.css">
+ <link rel="stylesheet" href="/mstyles.css">
 </head>
  <body>
     <div class="container">
