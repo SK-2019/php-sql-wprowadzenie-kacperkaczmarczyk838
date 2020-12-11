a571bf31a16ad105d17826f086622edc88c961df
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-11 21:38:06 +0100

    Update limit.php

--- zadania/limit.php
+++ zadania/limit.php
@@ -14,18 +14,36 @@
       <div class="item colorBlue">
       <h1>  MENU:</h1>
        <ul>
-        <li class="item_link"><a class="" href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">GITHUB</a></li>
-        <li class="item_link"><a class="" href="/danedobazy/danedobazy.php">Dane Do Bazy</a></li>
-        <li class="item_link"><a class="" href="/danedobazy/formularz.html">Formularz</a></li>
-        <li class="item_link"><a class="" href="/zadania/pracownicy.php">Pracownicy</a></li>
-        <li class="item_link"><a class="" href="/zadania/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
-        <li class="item_link"><a class="" href="/zadania/funkcAgregujace.php">Funkcje Agregujace</a></li>
-        <li class="item_link"><a class="" href="/zadania/sortowanie.php">Sorotwanie</a></li>
-        <li class="item_link"><a class="" href="/zadania/groupby.php">Group By</a></li>
-        <li class="item_link"><a class="" href="/zadania/having.php">Having</a></li>
-        <li class="item_link"><a class="" href="/zadania/dataiczas.php">Data i Czas</a></li>
-        <li class="item_link"><a class="" href="/zadania/function.php">Funkcja</a></li>
-        <li class="item_link"><a class="" href="/biblioteka/ksiazki.php">Książki</a></li>
+        <h6>
+	       <div>
+        <li class="item_link"><a class="" href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">☆ GITHUB ☆</a></li>
+         
+        <li class="item_link"><a class="" href="/index.php">★ STRONA GŁÓWNA ★</a></li>
+		      
+        <li class="item_link"><a class="" href="/danedobazy/danedobazy.php"> ✦ Dane Do Bazy ✦</a></li>
+	      
+        <li class="item_link"><a class="" href="/danedobazy/formularz.html"> ✧ Formularz ✧ </a></li>
+		   
+        <li class="item_link"><a class="" href="/zadania/pracownicy.php"> ✦ Pracownicy ✦</a></li>
+		
+        <li class="item_link"><a class="" href="/zadania/pracownicy_organizacja.php">✧ Pracownicy i Org. ✧</a></li>
+			
+        <li class="item_link"><a class="" href="/zadania/funkcAgregujace.php">✦ Funkcje Agregujace ✦</a></li>
+			
+        <li class="item_link"><a class="" href="/zadania/sortowanie.php">✧ Sorotwanie ✧</a></li>
+			
+        <li class="item_link"><a class="" href="/zadania/groupby.php"> ✦ Group By ✦ </a></li>
+		
+        <li class="item_link"><a class="" href="/zadania/having.php">✧ Having ✧</a></li>
+		
+        <li class="item_link"><a class="" href="/zadania/limit.php">✦ Limit ✦</a></li>
+		
+        <li class="item_link"><a class="" href="/zadania/dataiczas.php">✧ Data i Czas ✧</a></li>
+		
+        <li class="item_link"><a class="" href="/zadania/function.php">✦ Funkcja ✦</a></li>
+	
+			 </div> 
+	       </h6>
        </ul>
        
     </div>
