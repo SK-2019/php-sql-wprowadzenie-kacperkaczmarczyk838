485b2d33ba3f7eb4f3bab47c54f6e6cbb87c85dd
kacperkaczmarczyk838 <63092907+kacperkaczmarczyk838@users.noreply.github.com>
2020-12-11 13:21:50 +0100

    Update wypozyczone.php

--- biblioteka/wypozyczone.php
+++ biblioteka/wypozyczone.php
@@ -31,10 +31,10 @@
     </div>
       <div class="item colorGreen">
          
-    <h1>Wypożyczenie książki:</h1>
+    <h1>Dodawanie pracownika:</h1>
 
 <body>
-<form class="formularz" action="wypo.php" method="POST">
+<form class="formularz" action="insert.php" method="POST">
     <input type="text" name="imie" placeholder="imie"></br>
     <input type="text" name="dzial" placeholder="dzial"></br>
     <input type="text" name="zarobki" placeholder="zarobki"></br>
@@ -44,34 +44,40 @@
        
       <hr/>
        
+        <h1>Usuwanie pracownika:</h1>
+       
+<form action="delete.php" method="POST">
+       <input type="text" name="id" placeholder="ID"></br>
+   <input type="submit" value="Usuń">
+</form>
+</body>
+</head>
+</html>
 
 <?php
 
 echo("<hr />");
-       
-       require_once('../connect.php');
-    echo("<h1>Wszystkie książki (z możliwością oddania):</h1>");
-$sql = 'SELECT * FROM biblAutor, biblTytul, biblAutor_biblTytul WHERE biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id';
-
-$result=$conn->query($sql);
-    echo("<table border=1>");
-    echo("<th>id</th>");
-    echo("<th>autor</th>");
-    echo("<th>tytul</th>");
-        while($row=$result->fetch_assoc()){
-            echo("<tr>");
-                   echo("<td>".$row["id"]."</td><td>".$row["autor"]."</td><td>".$row["tytul"]."</td>");
-                   echo("<td><form action='odd.php' method=POST>");
-                   echo("<input type name='id' value='".$row['id_pracownicy']."'hidden>");
-                   echo("<input type='submit' value='Usuń'>");
-                   echo("</form></td>");
+    echo("<h1>Tabela Wszystkich Pracowników (z możliwością usuwania):</h1>");
+require_once('../connect.php');
+$result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');       
+        echo("<table>");      
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dział</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+        echo("<th>Usuwanie</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                    echo("<td><form action='delete.php' method=POST>");
+                     echo("<input type name='id' value='".$row['id_pracownicy']."'hidden>");
+                     echo("<input type='submit' value='Usuń'>");
+                    echo("</form></td>");
                 echo("</tr>");
             }
         echo("</table>");
-            echo("</tr>");
-        }
-    echo("</table>");
-       
            
 ?>
  </div>
