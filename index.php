f36e38da54aa948b106ccbaaf8ce9a9b60d1b056
Kacper <kacperkaczmarczyk1389@gmail.com>
2021-01-22 15:54:39 +0100

    zadanie

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html> 
 <head>
     <meta charset="UTF-8" />
- <title>Kacper Kaczmarczyk 2Ti</title>
+ <title>Kacper Jakub Kaczmarczyk 2Ti</title>
  <link rel="stylesheet" href="/mystyles.css">
 </head>
  <body>
