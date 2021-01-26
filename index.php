3f0264e2fc0ee2fa42e6007840a3a1684be851a3
Kacper <kacperkaczmarczyk1389@gmail.com>
2021-01-26 13:27:42 +0100

    commit

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function myFunction() {
 <?php
 echo("<h1>Strona Główna</h1>");
 echo("<h2>Tabela Wszystkich Pracowników:</h2>");
-require_once('/assets/connect.php');
+require_once('/connect.php');
 $result = $conn->query('SELECT * FROM `pracownicy`, `organizacja` WHERE dzial = id_org');       
         echo("<table>");      
         echo("<th>ID</th>");
