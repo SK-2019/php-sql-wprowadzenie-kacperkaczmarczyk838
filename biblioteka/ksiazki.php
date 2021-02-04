f3402c71d36b9cf37efe500b8e0b1bf7ee05b2b3
Kacper <kacperkaczmarczyk1389@gmail.com>
2021-02-04 11:46:42 +0100

    style.css

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -2,6 +2,7 @@
 <html> 
 <head>
  <title>Kacper Kaczmarczyk 2Ti gr. 2</title>
+ <link rel="stylesheet" href="/assets/mystyles.css">
  <link rel="icon" href="https://image.flaticon.com/icons/png/512/616/616430.png">
 </head>
  <body>
