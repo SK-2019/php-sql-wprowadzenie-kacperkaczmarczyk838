cd435b4c0bbdbcd310d4cf84ff3da365ce30cb62
Kacper <kacperkaczmarczyk1389@gmail.com>
2021-02-08 20:50:14 +0100

    body.html

--- pracownicy/having.php
+++ pracownicy/having.php
@@ -1,43 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Kacper Kaczmarczyk 2Ti gr. 2</title>
- <link rel="stylesheet" href="/assets/mystyles.css">
- <link rel="icon" href="https://image.flaticon.com/icons/png/512/616/616430.png">
-</head>
- 
- <body>
-    <div class="container">
-      <div class="item colorRed">
-        <h1> ☆ Kacper Kaczmarczyk 2Ti ☆</h1>
+<?php include "assets/body2.html" ?>
 
-        <h1 class="tltle"></h1>
-      </div>
-      <div class="item colorBlue">
-      <h1>  MENU:</h1>
-         <ul>
-        <h6>
-	       <div>
-           <li class="item_link"><a class="" href="https://github.com/SK-2019/php-sql-wprowadzenie-kacperkaczmarczyk838">☆ GITHUB ☆</a></li>		       		       
-           <li class="item_link"><a class="" href="/index.php"> ✦ Strona Główna  ✦</a></li>	
-        <li class="item_link"><a class="" href="/pracownicy/danedobazy.php"> ✦ Dane Do Bazy  ✦</a></li>
-        <li class="item_link"><a class="" href="/cwiczenia/formularz.html"> ✧ Formularz ✧ </a></li>
-        <li class="item_link"><a class="" href="/pracownicy/pracownicy.php"> ✦ Pracownicy ✦</a></li>		
-        <li class="item_link"><a class="" href="/pracownicy/pracownicy_organizacja.php">✧ Pracownicy i Org. ✧</a></li>			
-        <li class="item_link"><a class="" href="/pracownicy/funkcAgregujace.php">✦ Funkcje Agregujace ✦</a></li>			
-        <li class="item_link"><a class="" href="/pracownicy/sortowanie.php">✧ Sorotwanie ✧</a></li>			
-        <li class="item_link"><a class="" href="/pracownicy/groupby.php"> ✦ Group By ✦ </a></li>			
-        <li class="item_link"><a class="" href="/pracownicy/limit.php">✦ Limit ✦</a></li>		
-        <li class="item_link"><a class="" href="/pracownicy/dataiczas.php">✧ Data i Czas ✧</a></li>		
-        <li class="item_link"><a class="" href="/cwiczenia/function.php">✦ Funkcja ✦</a></li>		
-        <li class="item_link"><a class="" href="/biblioteka/ksiazki.php">✧ Biblioteka ✧ </a></li>
-	
-			 </div> 
-	       </h6>
-       </ul>
-       
-    </div>
-      <div class="item colorGreen">
 <?php
 
 require_once('../assets/connect.php');
