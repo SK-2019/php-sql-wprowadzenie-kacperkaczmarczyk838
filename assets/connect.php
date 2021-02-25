3c781ae3d457e00ef6c3af8f2ae72870c35b62c7
Kacper <kacperkaczmarczyk1389@gmail.com>
2021-02-25 14:08:54 +0100

    localhost

--- assets/connect.php
+++ assets/connect.php
@@ -1,9 +1,12 @@
 <?php
 
+$hostname = $_SERVER['HTTP_HOST'];
+if ($hostname === '127.0.0.1'){
+    require_once("config.php");}
+
     $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
     if ($conn->connect_error) {
         die("connection failed: ".mysqli_connect_error());
     }
-
 
 ?>
